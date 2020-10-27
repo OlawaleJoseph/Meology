68b5ec426200f13a4ada406b0c3f6785ad23d5a4
somoye123 <somoye.ayotunde@gmail.com>
2020-10-27 18:59:54 +0100

    add mapStateToProps function for SingleMeal page

--- src/pages/SingleMeal.js
+++ src/pages/SingleMeal.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import setLoadingStatus from '../redux/actions/loadingAction';
+import { loadMeals } from '../redux/actions/mealsAction';
 
 const SingleMeal = () => (
   <div />
 );
 
 const mapStateToProps = ({ meal, loading }) => ({ meal, loading });
+
+const mapDispatchToProps = { setLoadingStatus, loadMeals };
 
 export default SingleMeal;
