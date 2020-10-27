9aa04d98fc0460314fce8d285d199c0143590b20
somoye123 <somoye.ayotunde@gmail.com>
2020-10-27 16:28:42 +0100

    import loading component into MealList component

--- src/components/MealList.js
+++ src/components/MealList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Loading from './Loading';
 import setLoadingStatus from '../redux/actions/loadingAction';
 import { loadMeals } from '../redux/actions/mealsAction';
 
