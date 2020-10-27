b68aca1e885b46e197f596d91385cda9589594fe
somoye123 <somoye.ayotunde@gmail.com>
2020-10-27 16:41:39 +0100

    destructure props and add PropTypes validations

--- src/components/MealList.js
+++ src/components/MealList.js
@@ -1,15 +1,25 @@
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Loading from './Loading';
 import setLoadingStatus from '../redux/actions/loadingAction';
 import { loadMeals } from '../redux/actions/mealsAction';
 
-const MealList = () => (
+const MealList = ({
+  Meals, Loading, SearchTerm, setLoadingStatus, loadMeals,
+}) => (
   <section className="section">
     <h2 className="section-title">Meals</h2>
   </section>
 );
 
+MealList.propTypes = {
+  SearchTerm: PropTypes.string.isRequired,
+  Meals: PropTypes.arrayOf(PropTypes.object).isRequired,
+  Loading: PropTypes.bool.isRequired,
+  setLoadingStatus: PropTypes.func.isRequired,
+  loadMeals: PropTypes.func.isRequired,
+};
 const mapStateToProps = ({ SearchTerm, Meals, Loading }) => ({ SearchTerm, Meals, Loading });
 
 const mapDispatchToProps = { setLoadingStatus, loadMeals };
