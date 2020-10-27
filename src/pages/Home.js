724304effa273a22d4cfd513d8b65d6c4df2658f
somoye123 <somoye.ayotunde@gmail.com>
2020-10-27 15:10:37 +0100

    render mealList component in HomePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import SearchForm from '../components/SearchForm';
+import MealList from '../components/MealList';
 
 const Home = () => (
   <main>
     <SearchForm />
+    <MealList />
   </main>
 );
 
