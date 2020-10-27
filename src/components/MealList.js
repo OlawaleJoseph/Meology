9a748d6c0f69cb07d1ce837a9b939cdedaa7dce6
somoye123 <somoye.ayotunde@gmail.com>
2020-10-27 17:22:41 +0100

    add conditional in meallist for it content

--- src/components/MealList.js
+++ src/components/MealList.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Loading from './Loading';
+import LoadingBar from './Loading';
 import setLoadingStatus from '../redux/actions/loadingAction';
 import { loadMeals } from '../redux/actions/mealsAction';
+import Meal from './Meal';
 
 const MealList = ({
-  Meals, Loading, SearchTerm, setLoadingStatus, loadMeals,
+  Meals,
+  Loading,
+  SearchTerm,
+  setLoadingStatus,
+  loadMeals,
 }) => {
   useEffect(() => {
     setLoadingStatus(true);
@@ -14,9 +19,31 @@ const MealList = ({
     setLoadingStatus(false);
   }, [loadMeals, SearchTerm, setLoadingStatus]);
 
+  if (Loading) return <LoadingBar />;
+
+  if (Meals.length < 1) {
+    return (
+      <h2 className="section-title">no meals matched your search criteria</h2>
+    );
+  }
+
+  return (
     <section className="section">
       <h2 className="section-title">Meals</h2>
-    </section>;
+      <div className="meals-center">
+        {Meals.map(meal => (
+          <Meal
+            key={meal.id}
+            image={meal.image}
+            name={meal.name}
+            id={meal.id}
+            info={meal.info}
+            area={meal.area}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 MealList.propTypes = {
@@ -26,7 +53,11 @@ MealList.propTypes = {
   setLoadingStatus: PropTypes.func.isRequired,
   loadMeals: PropTypes.func.isRequired,
 };
-const mapStateToProps = ({ SearchTerm, Meals, Loading }) => ({ SearchTerm, Meals, Loading });
+const mapStateToProps = ({ SearchTerm, Meals, Loading }) => ({
+  SearchTerm,
+  Meals,
+  Loading,
+});
 
 const mapDispatchToProps = { setLoadingStatus, loadMeals };
 
