30f9e6573a9638b8499097f1fa6370c36c246ea7
somoye123 <somoye.ayotunde@gmail.com>
2020-10-27 11:36:35 +0100

    wrap entire application inside the ReduxProvider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './style.css';
+import { Provider as ReduxProvider } from 'react-redux';
+import App from './components/App';
+import ConfigureStore from './redux/configureStore';
 
-ReactDOM.render(
+const store = ConfigureStore();
+render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </ReduxProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
