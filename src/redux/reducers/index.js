ea9ff60552b0dd8d035d41d275670addf2eb6d3c
somoye123 <somoye.ayotunde@gmail.com>
2020-10-27 15:39:23 +0100

    add loading reducer to application root reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import SearchTerm from './searchTermReducer';
 import Meals from './mealsReducer';
+import Loading from './loadingReducer';
 
-export default combineReducers({ SearchTerm, Meals });
+export default combineReducers({ SearchTerm, Meals, Loading });
