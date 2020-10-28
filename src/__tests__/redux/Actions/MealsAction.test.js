c93d960a5c91016c766215513414598ab930ce30
somoye123 <somoye.ayotunde@gmail.com>
2020-10-28 12:50:04 +0100

    add test case for meals action creator

---
+++ src/__tests__/redux/Actions/MealsAction.test.js
@@ -0,0 +1,16 @@
+import * as types from '../../../redux/actions/actionTypes';
+import * as MealsAction from '../../../redux/actions/mealsAction';
+
+describe('Meals Action Creator', () => {
+  it('should create a SET_MEALs action', () => {
+    // arrange
+    const meals = null;
+    const expectedAction = { type: types.SET_MEALS, meals };
+
+    // act
+    const action = MealsAction.loadMealsSuccess(meals);
+
+    // assert
+    expect(action).toEqual(expectedAction);
+  });
+});
