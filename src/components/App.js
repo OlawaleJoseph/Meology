2427588dcb235c0e6ffdbd8c02f236cab5103ac6
somoye123 <somoye.ayotunde@gmail.com>
2020-10-29 10:20:52 +0100

    configure React-toastify for handling Error

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import HomePage from '../pages/Home';
 import AboutPage from '../pages/About';
 import SingleMealPage from '../pages/SingleMeal';
 import PageNotFound from '../pages/Error';
 import Navbar from './Navbar';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
   return (
@@ -16,6 +18,7 @@ export default function App() {
         <Route path="/meal/:id" component={SingleMealPage} />
         <Route component={PageNotFound} />
       </Switch>
+      <ToastContainer autoClose={3000} hideProgressBar />
     </>
   );
 }
